9a6ff0e0e6a42e1150cfbee901e15394bb399b1d
brixmavu <brixton@projectcodex.co>
2015-07-30 12:44:42 +0200

    modified mostPopular.js

--- mostPopular.js
+++ mostPopular.js
@@ -1,26 +1,10 @@
 var fs = require("fs");
 
 /*all items in Nelisa Sales History*/
-var findFiles = function (fileName) {
-   
-    var productList = []; 
-    var productMap = {};
+exports.linesInFiles = function (fileName) {
 
-        var fileContent = fs.readFileSync(fileName, 'utf8'); 
-        var products = fileContent.split("\r"); 
-     
-    products.forEach(function (product) { 
-      var hold = product.split(";");
-      var prodName = hold[2];
-      if(productMap[prodName] === undefined && prodName !== "stock item"){
-        productList.push(prodName);
-        productMap[prodName] = 0;  
-      };
-       
-    }); 
-
-console.log(productList)  
-return productList;
+	var productMap = findProductsSold(fileName);
+	return Object.keys(productMap);
 
 }
 
@@ -28,8 +12,7 @@ return productList;
 var findProductsSold = function ( fileName) {
     var productNames= [];
     var productsMap = {};
-    var quantitySold = [];
-         
+    var quantitySold = [];     
 
     var fileContent = fs.readFileSync(fileName, "utf8");
     var products = fileContent.split("\r");
@@ -47,48 +30,26 @@ var findProductsSold = function ( fileName) {
    			return;
    		}
 
-      if (productsMap[productName] === undefined ) {
-        quantitySold.push(productName);
-        productsMap[productName] = 0;   
-      }
+      	if (productsMap[productName] === undefined ) {
+        	quantitySold.push(productName);
+        	productsMap[productName] = 0;   
+      	}
 
       productsMap[productName] += quantity;
     
     });
           
-console.log(productsMap);
-return productsMap;
+	console.log(productsMap);
+	return productsMap;
 };
 
 
 /*most popular product sold*/
-var findMostPopular = function (fileName) {
+exports.popularProducts = function (fileName) {
 	// body...
+	var productsMap = findProductsSold(fileName);
+
 	var mostPopularProdct = {};
-    var productsMap = {};
-    var quantitySold = [];
-         
-
-    var fileContent = fs.readFileSync(fileName, "utf8");
-    var products = fileContent.split("\r");
-         
-
-    products.forEach( function (product) {
-          
-   	var productColumns = product.split(";");
-   	var productName = productColumns[2];
-   	var quantity = Number(productColumns[3]);
-
-        if (productsMap[productName] === undefined ) {
-            quantitySold.push(productName);
-            productsMap[productName] = 0; 
-        }
-
-       	productsMap[productName] += quantity;
-       
-        
-    });
-
     
     var max = 0;
     for(var prop in productsMap) {
@@ -102,28 +63,8 @@ var findMostPopular = function (fileName) {
             }
        }
     } 
-
-
-
     console.log(mostPopularProdct);
     return mostPopularProdct;
 }
 
-
-
-
-
-exports.linesInFiles = function(fileName){
-   var productList = findFiles(fileName);
-   return productList;
-};
-
-exports.productsSold = function(fileName){
-    var productsMap = findProductsSold(fileName)
-    return productsMap
-};
-
-exports.popularProducts = function(fileName){
-    var mostPopularProdct = findMostPopular(fileName)
-    return mostPopularProdct;
-};
+exports.productsSold = findProductsSold;
